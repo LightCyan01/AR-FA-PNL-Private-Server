8c49546d155beec6a1394c5bf2134045738bec38
Jaymie <jaymiearclight@gmail.com>
2026-09-17 14:49:33 -0400

    Decode compound resistance effects

--- src/state/combat/effects/tests/status_catalog.rs
+++ src/state/combat/effects/tests/status_catalog.rs
@@ -34,3 +34,37 @@ fn shared_status_rules_cover_textless_and_shorthand_skills() {
         );
     }
 }
+
+#[test]
+fn compound_resistance_effects_keep_each_attribute() {
+    for (effect_id, skill_id, target, attribute, state_id, duration) in [
+        (91000971, 11000436, "targets", 5, 50006, 5),
+        (91000936, 11000436, "targets", 6, 50007, 5),
+        (4000003, 31553, "enemies", 5, 50006, 2),
+        (4000004, 31553, "enemies", 6, 50007, 2),
+        (4000005, 31553, "enemies", 7, 50008, 2),
+        (4000006, 31553, "enemies", 8, 50009, 2),
+    ] {
+        let rule = rule_for(effect_id, "active", "skill", skill_id)
+            .unwrap()
+            .unwrap();
+        assert_eq!(
+            (
+                rule.operation.as_str(),
+                rule.target.as_str(),
+                rule.attack_attributes.as_slice(),
+                rule.state_id,
+                &rule.expiry,
+                rule.duration,
+            ),
+            (
+                "attribute_taken",
+                target,
+                [attribute].as_slice(),
+                state_id,
+                &Expiry::Attacked,
+                duration,
+            )
+        );
+    }
+}
